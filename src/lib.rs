6107b0d9e5cea18141c084a42016764cdb41ebe3
Luis Valdez <luis.valdez@affluences.com>
2020-02-10 21:59:50 +0100

    level 2

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use std::ops::Not;
 use std::path::Path;
 use std::fs::File;
-use std::io::{BufReader, BufRead};
+use std::io::{Write, BufReader, BufRead};
 
 #[cfg(test)]
 mod tests {
@@ -88,11 +88,35 @@ impl Image {
                 img.height = size[1];
             } else if count > 2 {
                 let pixels: Vec<u8> = get_numbers_from_line(l);
-                if pixels.len() != 3 { return None; }
-                img.pixels.push(Pixel::create(pixels[0], pixels[1], pixels[2]));
+                if pixels.len() % 3 != 0 { println!("len : {}", pixels.len()); return None; }
+                for idx in (0..pixels.len()).step_by(3) {
+                    img.pixels.push(Pixel::create(pixels[idx], pixels[idx + 1], pixels[idx + 2]));
+                }
             }
         }
         return Some(img);
+    }
+
+    pub fn save(&self, filename: &Path) {
+        let mut str: String = format!("P3\n{} {}\n255\n", self.width, self.height);
+
+        for p in self.pixels.iter() {
+            str = str + format!("{} {} {}\n", p.r, p.g, p.b).as_ref();
+        }
+
+        let mut f = File::create(filename).expect("error");
+        f.write_all(str.as_ref());
+    }
+
+    pub fn grayscale(&mut self) {
+        for  p in self.pixels.iter_mut() {
+            p.grayscale();
+        }
+    }
+    pub fn revert(&mut self) {
+        for p in self.pixels.iter_mut() {
+            p.revert();
+        }
     }
 }
 
