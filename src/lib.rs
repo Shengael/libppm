4a7e5c12219334ecfe64f6e658015208480a4333
Patrick ZHUANG <patrickzhuang@hotmail.fr>
2020-02-13 23:00:40 +0100

    converted to rustdoc comment style

--- src/lib.rs
+++ src/lib.rs
@@ -39,44 +39,38 @@ pub struct Image {
 
 /// Implements functions for the Pixel structure
 impl Pixel {
-    /*
-        Constructor, return a Pixel with the three colors in arguments. Each color is a u8
-    */
+    
+    /// Constructor, return a Pixel with the three colors in arguments. Each color is a u8
     pub fn create(r: u8, g: u8, b: u8) -> Pixel {
         Pixel { r, g, b }
     }
-    /*
-        ToString() of Pixel structure; Display each colors of the Pixel
-    */
+
+    /// ToString() of Pixel structure; Display each colors of the Pixel
     pub fn display(&self) {
         println!("red: {} green: {} blue: {}", self.r, self.g, self.b);
     }
 
-    /*
-        Revert each color of the Pixel
-        # Arguments
-        * self - Current Pixel, Pixel to be reverted
-        
-        # Example 
-        If current Pixel's color are (155, 145, 135) 
-        current's Pixel's color will become (100, 110, 120)
-    */
+    /// Revert each color of the Pixel
+    /// # Arguments
+    /// * self - Current Pixel, Pixel to be reverted
+    /// 
+    /// # Example 
+    /// If current Pixel's color are (155, 145, 135) 
+    /// current's Pixel's color will become (100, 110, 120)
     pub fn revert(&mut self) {
         self.r = 255 - self.r;
         self.g = 255 - self.g;
         self.b = 255 - self.b;
     }
 
-    /*
-        Transform current Pixel into a grayscale Pixel
-        Each color will take the same value of grayscale (between 0 and 255)
-        # Arguments
-        * self - Current Pixel, Pixel to be converted
-        
-        # Example 
-        If current Pixel's color are (179, 120, 141) 
-        current's Pixel's color will become (140, 140, 140)
-    */
+    /// Transform current Pixel into a grayscale Pixel
+    /// Each color will take the same value of grayscale (between 0 and 255)
+    /// # Arguments
+    /// * self - Current Pixel, Pixel to be converted
+    /// 
+    /// # Example 
+    /// If current Pixel's color are (179, 120, 141) 
+    /// current's Pixel's color will become (140, 140, 140)
     pub fn grayscale(&mut self) {
         let gray = (self.r as f32 * 0.3 + self.g as f32 * 0.59 + self.b as f32 * 0.11) as u8;
         self.r = gray;
@@ -92,53 +86,48 @@ impl Pixel {
 impl Not for Pixel {
     type Output = Pixel;
 
-    /*
-        Return a reverted version of a Pixel
-        # Arguments
-        * self - Pixel to be converted
-        
-        # Example 
-        If Pixel's color are (155, 145, 135) 
-        Pixel's color will become (100, 110, 120)
-    */
+    /// Return a reverted version of a Pixel
+    /// # Arguments
+    /// * self - Pixel to be converted
+    /// 
+    /// # Example 
+    /// If Pixel's color are (155, 145, 135) 
+    /// Pixel's color will become (100, 110, 120)
     fn not(self) -> Self::Output {
         return Pixel { r: 255 - self.r, g: 255 - self.g, b: 255 - self.b };
     }
 }
 
 impl PartialEq for Pixel {
-    /*
-        Equals to determine if the two Pixel in parameters are equals.
-        Return true if self and other and equals else return false
-        (the r, g and b of self are equals to the r, g and b of other)
-        
-        # Arguments
-        * self - a Color to be compared
-        * other - a second Color to compare the first one
-        
-        # Return
-        * bool - corresponding to the equality (or not) of the two arguments
-    */
+
+    /// Equals to determine if the two Pixel in parameters are equals.
+    /// Return true if self and other and equals else return false
+    /// (the r, g and b of self are equals to the r, g and b of other)
+    /// 
+    /// # Arguments
+    /// * self - a Color to be compared
+    /// * other - a second Color to compare the first one
+    /// 
+    /// # Return
+    /// * bool - corresponding to the equality (or not) of the two arguments
     fn eq(&self, other: &Self) -> bool {
         return self.r == other.r && self.g == other.g && self.b == other.b;
     }
 }
 
 impl Image {
-    // Constructor, return an Image
+    /// Constructor, return an Image
     pub fn create(width: i32, height: i32, pixels: Vec<Pixel>) -> Image {
         Image { width, height, pixels }
     }
 
-    /*
-         Create a new Image from .ppm File
-         # Arguments
-         * filename: &Path - The path corresponding to the file to be read.
-
-         # Return
-         * Option<Image> - The Image created through the file read. Use Optional
-                           in case there is an error during file reading
-    */
+    /// Create a new Image from .ppm File
+    /// # Arguments
+    /// * filename: &Path - The path corresponding to the file to be read.
+    ///    
+    /// # Return
+    /// * Option<Image> - The Image created through the file read. Use Optional
+    ///                   in case there is an error during file reading
     pub fn new_with_file(filename: &Path) -> Option<Image> {
         let mut img = Image {
             width: 0,
@@ -179,12 +168,10 @@ impl Image {
         return Some(img);
     }
 
-    /*
-        Write current Image in a file
-        # Arguments
-        * self: Current Image to be written in file
-        * filename: Filename of file for Image to be written in
-    */
+    /// Write current Image in a file
+    /// # Arguments
+    /// * self: Current Image to be written in file
+    /// * filename: Filename of file for Image to be written in
     pub fn save(&self, filename: &Path) {
         // Convert current Image into a String to be written in the file
 
@@ -204,42 +191,36 @@ impl Image {
     }
 
 
-    /*
-        Convert current RGB Image to a grayscale Image.
-        
-        # Arguments
-        * image:self - Current Image, Image to be converted
-    */
+    /// Convert current RGB Image to a grayscale Image.
+    /// 
+    /// # Arguments
+    /// * image:self - Current Image, Image to be converted
     pub fn grayscale(&mut self) {
         for  p in self.pixels.iter_mut() {
             p.grayscale();
         }
     }
 
-    /*
-        Convert current RGB Image to the reverted version of current Image.
-        
-        # Arguments
-        * image:self - Current Image, Image to be converted
-    */
+    /// Convert current RGB Image to the reverted version of current Image.
+    /// 
+    /// # Arguments
+    /// * image:self - Current Image, Image to be converted
     pub fn revert(&mut self) {
         for p in self.pixels.iter_mut() {
             p.revert();
         }
     }
 
-    /*
-        Equals to determine if the two Image in parameters are equals.
-        Return true if self and other and equals else return false
-        (Same dimensions and pixels array in each Image)
-        
-        # Arguments
-        * self - current Image to be compared
-        * Image - a second Image to compare the first one to
-        
-        # Return
-        * bool - corresponding to the equality (or not) of the two arguments
-    */
+    /// Equals to determine if the two Image in parameters are equals.
+    /// Return true if self and other and equals else return false
+    /// (Same dimensions and pixels array in each Image)
+    /// 
+    /// # Arguments
+    /// * self - current Image to be compared
+    /// * Image - a second Image to compare the first one to
+    /// 
+    /// # Return
+    /// * bool - corresponding to the equality (or not) of the two arguments
     pub fn equal(&mut self, i: &mut Image) -> bool {
         if self.width != i.width || self.height != i.height {
             return false;
@@ -256,12 +237,10 @@ impl Image {
     }
 }
 
-/*
-    Transform a String with numbers in it into 
-    a Vector<u32> for the picture pixels.
-    # Example :
-    * "100 124 45" passed as parameters will return Vec{100, 124, 45}. 
-*/
+/// Transform a String with numbers in it into 
+/// a Vector<u32> for the picture pixels.
+/// # Example :
+/// * "100 124 45" passed as parameters will return Vec{100, 124, 45}. 
 pub fn get_numbers_from_line(line: String) -> Vec<u8> {
     line.split('#').collect::<Vec<&str>>()[0].split_ascii_whitespace()
         .map(|col| col.parse::<u8>())
@@ -270,18 +249,16 @@ pub fn get_numbers_from_line(line: String) -> Vec<u8> {
 }
 
 
-/*
-        Split whitespace in line in arguments 
-        
-        # Arguments
-        * line - Line to be splitted
-        
-        # Return
-        * Vec<i32> - Array of each string in line
-
-        # Exemple
-        * "2 3" passed as parameters will return Vec{}
-    */
+/// Split whitespace in line in arguments 
+/// 
+/// # Arguments
+/// * line - Line to be splitted
+/// 
+/// # Return
+/// * Vec<i32> - Array of each string in line
+/// 
+/// # Exemple
+/// * "2 3" passed as parameters will return Vec{}
 pub fn get_size(line: String) -> Vec<i32> {
     line.split("#").collect::<Vec<&str>>()[0].split_ascii_whitespace()
         .map(|col| col.parse::<i32>())
